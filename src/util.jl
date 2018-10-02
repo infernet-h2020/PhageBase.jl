f9bf58e1d27cd2f129bb6220dc567c1fa050f88a
Jorge Fernandez-de-Cossio-Diaz <j.cossio.diaz@gmail.com>
2018-10-01 21:16:22 -0400

    hoy

--- src/util.jl
+++ src/util.jl
@@ -23,8 +23,8 @@ macro checknonnegint(X::Union{Expr,Symbol}...)
 end
 
 "accurate log(1 + exp(x))"
-@inline log1pexp(x::AbstractFloat) = x ≤ -37 ? exp(x) : x ≤ 18 ? log1p(exp(x)) : x ≤ 33.3 ? x + exp(-x) : x;
-@inline log1pexp(x::Real) = log1pexp(float(x))
+@inline log1pexp(x::Real) = x ≤ -37 ? exp(x) : x ≤ 18 ? log1p(exp(x)) : x ≤ 33.3 ? x + exp(-x) : x;
+#@inline log1pexp(x::Real) = log1pexp(float(x))
 
 "Fermi-Dirac binding probability"
 fermi_dirac_prob(φ::Real) = 1 / (1 + exp(-φ))
@@ -36,4 +36,4 @@ fermi_dirac_l1mp(φ::Real) = fermi_dirac_logp(-φ) # log(1-p)
 fermi_dirac_prob(μ::Real, E::Real) = fermi_dirac_prob(μ - E)
 fermi_dirac_logp(μ::Real, E::Real) = fermi_dirac_logp(μ - E)
 fermi_dirac_1mp(μ::Real, E::Real) = fermi_dirac_1mp(μ - E)
-fermi_dirac_l1mp(μ::Real, E::Real) = fermi_dirac_l1mp(μ::Real, E::Real)
+fermi_dirac_l1mp(μ::Real, E::Real) = fermi_dirac_l1mp(μ - E)
