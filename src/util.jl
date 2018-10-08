854c1a9596b0f7da71fc40851751ec5eaf7a27b9
Jorge Fernandez-de-Cossio-Diaz <j.cossio.diaz@gmail.com>
2018-10-07 23:52:48 -0400

    StatsFuns

--- src/util.jl
+++ src/util.jl
@@ -1,6 +1,10 @@
+using StatsFuns
+
+
 export @checkposint, @checknonnegint
 export fermi_dirac_prob, fermi_dirac_1mp, fermi_dirac_logp, fermi_dirac_l1mp
 export log1pexp, xlogx, xexpx
+
 
 "throws an error if an argument is not a positive integer"
 macro checkposint(X::Union{Expr,Symbol}...)
@@ -23,24 +27,18 @@ macro checknonnegint(X::Union{Expr,Symbol}...)
 end
 
 
-"accurate log(1 + exp(x))"
-@inline function log1pexp(z::Real)
-	x = float(z)
-	x ≤ -37 ? exp(x) : x ≤ 18 ? log1p(exp(x)) : x ≤ 33.3 ? x + exp(-x) : x;
-end
+#= Now taking this stuff from StatsFuns =#
+# "accurate log(1 + exp(x))"
+# @inline function log1pexp(z::Real)
+# 	x = float(z)
+# 	x ≤ -37 ? exp(x) : x ≤ 18 ? log1p(exp(x)) : x ≤ 33.3 ? x + exp(-x) : x;
+# end
 
-
-"x * log(x), giving zero if x == 0"
-@inline xlogx(x::AbstractFloat) = x == 0 ? zero(x) : x * log(x)
+# "x * log(x), giving zero if x == 0"
+# @inline xlogx(x::Real) = iszero(x) ? float(x) : x * log(x)
 
 "x * exp(x), giving zero if x == -Inf"
-@inline xexpx(x::AbstractFloat) = x == -Inf ? zero(x) : x * exp(x)
-
-for f in (:xlogx, :xexpx)
-	@eval begin
-		($f)(x::Real) = ($f)(float(x))
-	end
-end
+@inline xexpx(x::Real) = x == -Inf ? zero(float(x)) : x * exp(x)
 
 
 "Fermi-Dirac binding probability"
--- test/util.jl
+++ test/util.jl
@@ -2,10 +2,10 @@ using Random
 
 
 @testset "xlogx, xexpx" begin
-    @test iszero(xlogx(0))
+    #@test iszero(xlogx(0))
 
-    @test xlogx(0) isa Float64
-    @test xlogx(Float16(0)) isa Float16
+    #@test xlogx(0) isa Float64
+    #@test xlogx(Float16(0)) isa Float16
 
     @test iszero(xexpx(-Inf))
 
@@ -16,27 +16,27 @@ using Random
         Random.seed!(testid + 160)
         x = 10rand()
         iszero(x) && continue   # should never happen
-        @test xlogx(x) ≈ x * log(x)
+        #@test xlogx(x) ≈ x * log(x)
         @test xexpx(x) ≈ x * exp(x)
     end
 end
 
 
-@testset "log1pexp" begin
+# @testset "log1pexp" begin
 
-    @test log1pexp(1) isa Float64
-    @test log1pexp(Float16(1)) isa Float16
-    @test log1pexp(Int(1)) isa Float64
+#     @test log1pexp(1) isa Float64
+#     @test log1pexp(Float16(1)) isa Float16
+#     @test log1pexp(Int(1)) isa Float64
 
-    @test log1pexp(1) ≈ 1.31326168751822283404899549496785564191528008567034837471
-    @test log1pexp(-1) ≈ 0.313261687518222834048995494967855641915280085670348374719
+#     @test log1pexp(1) ≈ 1.31326168751822283404899549496785564191528008567034837471
+#     @test log1pexp(-1) ≈ 0.313261687518222834048995494967855641915280085670348374719
     
-    # test all branches
-    @test log1pexp(-40) ≈ 4.24835425529158898630497784363158218187775067982297454220e-18
-    @test log1pexp(0) ≈ 0.69314718055994530941723212145817656807550013436025525412068000
-    @test log1pexp(20) ≈ 20.000000002061153620314380703238982798877915235602669875387647193
-    @test log1pexp(40) ≈ 40.0000000000000000042483542552915889863049778436315821818777506798
-end
+#     # test all branches
+#     @test log1pexp(-40) ≈ 4.24835425529158898630497784363158218187775067982297454220e-18
+#     @test log1pexp(0) ≈ 0.69314718055994530941723212145817656807550013436025525412068000
+#     @test log1pexp(20) ≈ 20.000000002061153620314380703238982798877915235602669875387647193
+#     @test log1pexp(40) ≈ 40.0000000000000000042483542552915889863049778436315821818777506798
+# end
 
 
 @testset "Fermi Dirac binding probability" begin
